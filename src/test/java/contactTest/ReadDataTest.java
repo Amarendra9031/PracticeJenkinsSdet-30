8f954354cdf804bac2a496d6bc60769a1764cb3c
Amarendra kumar <Amarendra kumar@DESKTOP-Q0RMI1I>
2022-03-15 14:40:03 +0530

    Read the Data

--- src/test/java/contactTest/ReadDataTest.java
+++ src/test/java/contactTest/ReadDataTest.java
@@ -7,7 +7,7 @@ public class ReadDataTest
 	@Test
 	public void readDataTest()
 	{
-		String BROWSER = System.getProperty("browser");
+		String BROWSER = System.getProperty("browser1");
 		System.out.println(BROWSER);
 
 		String URL = System.getProperty("url");
