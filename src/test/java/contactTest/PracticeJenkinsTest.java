50b1ea363f5f072f5c52dca21455ba804b7e9d9b
Amarendra kumar <Amarendra kumar@DESKTOP-Q0RMI1I>
2022-03-15 16:14:12 +0530

    Commit Third

---
+++ src/test/java/contactTest/PracticeJenkinsTest.java
@@ -0,0 +1,12 @@
+package contactTest;
+
+import org.testng.annotations.Test;
+
+public class PracticeJenkinsTest 
+{
+	@Test
+	public void practiceJenkinsTest() 
+	{
+		System.out.println("Welcome to tys");
+	}
+}
